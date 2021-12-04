a3cf4b9a796e1968effda10d1b3a718b1b888950
armin <armin@learnwithsocrates>
2021-12-05 00:26:55 +0900

    fix all bugs

--- src/Constants/KEYS.js
+++ src/Constants/KEYS.js
@@ -1,2 +1,3 @@
-exports.DATABASE_URL = 'mongodb+srv://admin:Admin@123@cluster0.vskcf.mongodb.net/ElephantId-Tech?retryWrites=true&w=majority'
+// exports.DATABASE_URL = 'mongodb+srv://admin:Admin@123@cluster0.vskcf.mongodb.net/ElephantId-Tech?retryWrites=true&w=majority'
+exports.DATABASE_URL = 'mongodb+srv://electricbench:1234qwer@cluster0.lv1ia.mongodb.net/Loxodont?retryWrites=true&w=majority'
 exports.STORAGE_PATH = "/Users/mac/Documents/ElephantIdTech-Backend/ElephantId-Tech/upload";
--- src/Controllers/UserController.js
+++ src/Controllers/UserController.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { SignUp, Login,ChangePassword } = require('../Routes/User');
-const {USER_LOGIN, USER_SIGNUP,FORGET_PASSWORD } = require('../Constants/Routes');
+const { SignUp, Login,ChangePassword, GetAllUsers } = require('../Routes/User');
+const {USER_LOGIN, USER_SIGNUP,FORGET_PASSWORD, USER_GET_ALL } = require('../Constants/Routes');
 
 exports.LOGIN = router.post(USER_LOGIN,Login);
 exports.SIGNUP = router.post(USER_SIGNUP,SignUp);
 exports.CHANGE_PASSWORD = router.post(FORGET_PASSWORD,ChangePassword);
+exports.GET_ALL_USER =  router.post(USER_GET_ALL,GetAllUsers)
